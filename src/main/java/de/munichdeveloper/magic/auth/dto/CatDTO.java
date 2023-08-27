a02d196aeb40c81315287bb069baa22314f3f6f5
J <johannes.hinkov@gmail.com>
2023-08-27 22:29:44 +0200

    fix issue on deserialization

--- src/main/java/de/munichdeveloper/magic/auth/dto/CatDTO.java
+++ src/main/java/de/munichdeveloper/magic/auth/dto/CatDTO.java
@@ -1,8 +1,10 @@
 package de.munichdeveloper.magic.auth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class CatDTO {
     private String name;
 }
